09c36bc92dafa6946d8181122a9b004d13509e97
Colin Meret <colinmeret@gmail.com>
2015-08-07 14:07:26 -0400

    refactored to be without a directive

--- app.js
+++ app.js
@@ -2,25 +2,32 @@ var app = angular.module('cmTable', []);
 
 app
 .controller('mainController', ['$scope', function($scope) {
-  $scope.dogSpecies = {
-    rows: [
+  $scope.dogSpecies = [
       {name: 'scottish terrier', pattern: 'solid', size: 'small', personality: 'energetic'},
       {name: 'golden retriever', pattern: 'solid', size: 'large'},  
       {name: 'dalmation', pattern: 'spotted', size: 'medium'}  
-    ]
-  };
-  console.log($scope);
+    ];
 }])
 .directive('cmTable', [function() {
   return {
     restrict: 'A',
-    templateUrl: './cmtable.html',
     scope: {
-      cmTableData: '=' 
+      cmTableData: '='
     },
     link: function(scope, elem, attrs) {
-      scope.tabledata = scope.cmTableData;
-      console.log(scope.dogSpecies);
+      var $th = elem.find('th'); 
+      var attrsArray = [];
+      $($th).each(function(th) {
+        attrsArray.push($(this).attr('cm-table-param'));
+      });
+      var tbody = elem.find('tbody'); 
+      scope.cmTableData.forEach(function(item) {
+        var tr = angular.element('<tr></tr>');
+        tbody.append(tr);
+        attrsArray.forEach(function(attribute) {
+          tr.append('<td>' + item[attribute] + '</td>');
+        });
+      });
       scope.sortBy = function sortBy(key) {
         if(scope.prop === key) return scope.reverse = !scope.reverse;
         scope.prop = key;
